6c537d744bd933e32baee2a0a6795e3ca5093aa1
David Rohr <drohr@jwdt.org>
2025-06-11 09:26:49 +0200

    GPU Standalone: CI build should use -Werror

--- GPU/GPUTracking/Standalone/cmake/build.sh
+++ GPU/GPUTracking/Standalone/cmake/build.sh
@@ -7,13 +7,16 @@ set -e
 mkdir -p standalone/build
 pushd standalone/build
 cp $1/GPU/GPUTracking/Standalone/cmake/config.cmake .
-cat >> config.cmake << "EOF"
-set(ENABLE_CUDA 1)
-set(ENABLE_HIP 1)
-set(ENABLE_OPENCL 1)
-set(GPUCA_CONFIG_ONNX 1)
-set(GPUCA_BUILD_EVENT_DISPLAY 0)
+if [[ $GPUCA_STANDALONE_CI == 1 ]]; then
+  cat >> config.cmake << "EOF"
+  set(ENABLE_CUDA 1)
+  set(ENABLE_HIP 1)
+  set(ENABLE_OPENCL 1)
+  set(GPUCA_CONFIG_ONNX 1)
+  set(GPUCA_BUILD_EVENT_DISPLAY 0)
+  set(GPUCA_CONFIG_WERROR 1)
 EOF
+fi
 cmake -DCMAKE_INSTALL_PREFIX=../ $1/GPU/GPUTracking/Standalone
 make ${JOBS+-j $JOBS} install
 popd
