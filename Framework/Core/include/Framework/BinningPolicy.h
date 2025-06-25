19e73ce40c0dfa6fc33cfc373e572049c98aa43d
Vít Kučera <vit.kucera@cern.ch>
2025-06-25 10:23:32 +0200

    IWYU: BinningPolicy.h (#14323)

--- Framework/Core/include/Framework/BinningPolicy.h
+++ Framework/Core/include/Framework/BinningPolicy.h
@@ -12,8 +12,13 @@
 #ifndef FRAMEWORK_BINNINGPOLICY_H
 #define FRAMEWORK_BINNINGPOLICY_H
 
+#include "Framework/ASoA.h"
 #include "Framework/HistogramSpec.h" // only for VARIABLE_WIDTH
-#include "Framework/Pack.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <tuple>
+#include <vector>
 
 namespace o2::framework
 {
